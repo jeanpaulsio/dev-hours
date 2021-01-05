fe2141cd990f1471c56bf0e1e7f8596b635bbb1b
JP Sio <hello@jpsio.me>
2021-01-04 16:51:48 -0800

    Add more clocks

--- src/App.js
+++ src/App.js
@@ -3,32 +3,77 @@ import ClockWithOffset from "./ClockWithOffset";
 import "react-clock/dist/Clock.css";
 import "./App.css";
 
+const LA = [
+  { name: "Manuel", startHours: 1, endHours: 10 },
+  { name: "John", startHours: 6, endHours: 13 },
+  { name: "Jp", startHours: 9, endHours: 18 },
+  { name: "Dev", startHours: 21, endHours: 29 },
+];
+
+// Use Los Angeles as the pivot
+function generateData({ offset }) {
+  return [0, 1, 2, 3].map((i) => ({
+    name: LA[i].name,
+    startHours: LA[i].startHours + offset,
+    endHours: LA[i].endHours + offset,
+  }));
+}
+
 function App() {
   return (
     <div className="app">
-      <h2>New York</h2>
+      <h2>Los Angeles, USA</h2>
       <div className="clock-row">
-        <ClockWithOffset
-          offset="America/New_York"
-          abbrev="ny"
-          name="John"
-          startHours={9}
-          endHours={16}
-        />
-        <ClockWithOffset
-          offset="America/New_York"
-          abbrev="ny"
-          name="Manuel"
-          startHours={4}
-          endHours={13}
-        />
-        <ClockWithOffset
-          offset="America/New_York"
-          abbrev="ny"
-          name="Dev"
-          startHours={0}
-          endHours={8}
-        />
+        {generateData({ offset: 0 }).map((employee) => (
+          <ClockWithOffset
+            key={employee.name}
+            offset="America/Los_Angeles"
+            abbrev="la"
+            name={employee.name}
+            startHours={employee.startHours}
+            endHours={employee.endHours}
+          />
+        ))}
+      </div>
+      <h2>New York, USA</h2>
+      <div className="clock-row">
+        {generateData({ offset: 3 }).map((employee) => (
+          <ClockWithOffset
+            key={employee.name}
+            offset="America/New_York"
+            abbrev="ny"
+            name={employee.name}
+            startHours={employee.startHours}
+            endHours={employee.endHours}
+          />
+        ))}
+      </div>
+      <h2>Seville, Spain</h2>
+      <div className="clock-row">
+        {generateData({ offset: 9 }).map((employee) => (
+          <ClockWithOffset
+            key={employee.name}
+            offset="Europe/Madrid"
+            abbrev="eu"
+            name={employee.name}
+            startHours={employee.startHours}
+            endHours={employee.endHours}
+          />
+        ))}
+      </div>
+      <h2>Bengaluru, India</h2>
+      <div className="clock-row">
+        {generateData({ offset: 13 }).map((employee) => (
+          <ClockWithOffset
+            key={employee.name}
+            offset="Asia/Kolkata"
+            abbrev="as"
+            name={employee.name}
+            startHours={employee.startHours}
+            endHours={employee.endHours}
+            minutesOffset={30}
+          />
+        ))}
       </div>
     </div>
   );
--- src/ClockWithOffset.js
+++ src/ClockWithOffset.js
@@ -1,11 +1,28 @@
 import { useEffect, useState } from "react";
 
-import { startOfDay, addHours, isWithinInterval, format } from "date-fns";
+import {
+  startOfDay,
+  addHours,
+  addMinutes,
+  isWithinInterval,
+  format,
+} from "date-fns";
 import { utcToZonedTime, zonedTimeToUtc } from "date-fns-tz";
 
 import Clock from "react-clock";
 
-function ClockWithOffset({ offset, abbrev, name, startHours, endHours }) {
+ClockWithOffset.defaultProps = {
+  minutesOffset: 0,
+};
+
+function ClockWithOffset({
+  offset,
+  abbrev,
+  name,
+  startHours,
+  endHours,
+  minutesOffset,
+}) {
   const [currentTime, setCurrentTime] = useState(
     utcToZonedTime(new Date(), offset)
   );
@@ -21,29 +38,64 @@ function ClockWithOffset({ offset, abbrev, name, startHours, endHours }) {
     };
   }, [offset]);
 
+  function isOnline() {
+    const startTime = getStartTimeWithOffset();
+    const endTime = getEndTimeWithOffset();
+    const time = zonedTimeToUtc(currentTime, offset);
+    return isWithinInterval(time, { start: startTime, end: endTime });
+  }
+
+  /**
+   * Use the ID names to override the clock face styles in react-clock library
+   *
+   * Each ID is specific to a dev's hours as listed in:
+   * https://www.notion.so/getmainstreet/Team-bee08c85c2ff438a9dd15edc38755356
+   *
+   * Yes, I realized there is probably a better way to do this without creating
+   * a ton of css ids
+   */
   function getId() {
-    const startTime = getStartTime();
-    const endTime = getEndTime();
-    return isWithinInterval(currentTime, { start: startTime, end: endTime })
+    return isOnline()
       ? `${name.toLowerCase()}-${abbrev}-online`
       : `${name.toLowerCase()}-${abbrev}-offline`;
   }
 
+  function getStartTimeWithOffset() {
+    return zonedTimeToUtc(
+      addMinutes(addHours(startOfDay(new Date()), startHours), minutesOffset),
+      offset
+    );
+  }
+
+  function getEndTimeWithOffset() {
+    return zonedTimeToUtc(
+      addMinutes(addHours(startOfDay(new Date()), endHours), minutesOffset),
+      offset
+    );
+  }
+
   function getStartTime() {
-    return zonedTimeToUtc(addHours(startOfDay(new Date()), startHours), offset);
+    return addMinutes(
+      addHours(startOfDay(new Date()), startHours),
+      minutesOffset
+    );
   }
 
   function getEndTime() {
-    return zonedTimeToUtc(addHours(startOfDay(new Date()), endHours), offset);
+    return addMinutes(
+      addHours(startOfDay(new Date()), endHours),
+      minutesOffset
+    );
   }
 
   // console.log(currentTime, "\n", getStartTime(), "\n", getEndTime());
 
   return (
     <div id={getId()} className="clock-wrapper">
-      <Clock renderNumbers value={currentTime} />
+      <Clock size={100} renderNumbers value={currentTime} />
       <p>
-        {name}: {format(getStartTime(), "ha")} - {format(getEndTime(), "ha")}
+        <strong>{name}:</strong> {format(getStartTime(), "h:mm a")} -{" "}
+        {format(getEndTime(), "h:mma")} {isOnline() ? "🟢" : "❌"}
       </p>
     </div>
   );
