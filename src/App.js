6247a1242e2ac2570a911e97dd760cca03b12e9b
JP Sio <hello@jpsio.me>
2021-01-05 01:42:33 -0800

    Show one time zone at at timeg

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ClockWithOffset from "./ClockWithOffset";
 
 import "react-clock/dist/Clock.css";
@@ -21,65 +22,120 @@ function generateData({ offset }) {
 }
 
 function App() {
+  const [currentLocation, setCurrentLocation] = useState("America/New_York");
+
+  function renderRow() {
+    if (currentLocation === "America/Los_Angeles") {
+      return (
+        <>
+          <h2>Los Angeles, USA</h2>
+          <div className="clock-row">
+            {generateData({ offset: 0 }).map((employee) => (
+              <ClockWithOffset
+                key={employee.name}
+                offset="America/Los_Angeles"
+                abbrev="la"
+                name={employee.name}
+                startHours={employee.startHours}
+                endHours={employee.endHours}
+                hoursAvailable={employee.hoursAvailable}
+              />
+            ))}
+          </div>
+        </>
+      );
+    } else if (currentLocation === "America/New_York") {
+      return (
+        <>
+          <h2>New York, USA</h2>
+          <div className="clock-row">
+            {generateData({ offset: 3 }).map((employee) => (
+              <ClockWithOffset
+                key={employee.name}
+                offset="America/New_York"
+                abbrev="ny"
+                name={employee.name}
+                startHours={employee.startHours}
+                endHours={employee.endHours}
+                hoursAvailable={employee.hoursAvailable}
+              />
+            ))}
+          </div>
+        </>
+      );
+    } else if (currentLocation === "Europe/Madrid") {
+      return (
+        <>
+          <h2>Seville, Spain</h2>
+          <div className="clock-row">
+            {generateData({ offset: 9 }).map((employee) => (
+              <ClockWithOffset
+                key={employee.name}
+                offset="Europe/Madrid"
+                abbrev="eu"
+                name={employee.name}
+                startHours={employee.startHours}
+                endHours={employee.endHours}
+                hoursAvailable={employee.hoursAvailable}
+              />
+            ))}
+          </div>
+        </>
+      );
+    } else if (currentLocation === "Asia/Kolkata") {
+      return (
+        <>
+          <h2>Bengaluru, India</h2>
+          <div className="clock-row">
+            {generateData({ offset: 13 }).map((employee) => (
+              <ClockWithOffset
+                key={employee.name}
+                offset="Asia/Kolkata"
+                abbrev="as"
+                name={employee.name}
+                startHours={employee.startHours}
+                endHours={employee.endHours}
+                minutesOffset={30}
+                hoursAvailable={employee.hoursAvailable}
+              />
+            ))}
+          </div>
+        </>
+      );
+    }
+  }
+
   return (
     <div className="app">
-      <h2>Los Angeles, USA</h2>
-      <div className="clock-row">
-        {generateData({ offset: 0 }).map((employee) => (
-          <ClockWithOffset
-            key={employee.name}
-            offset="America/Los_Angeles"
-            abbrev="la"
-            name={employee.name}
-            startHours={employee.startHours}
-            endHours={employee.endHours}
-            hoursAvailable={employee.hoursAvailable}
-          />
-        ))}
+      <h1>Which devs are online? 🤔</h1>
+      <h2>Set your timezone - where are you closest to?</h2>
+      <div className="button-row">
+        <button
+          className="btn"
+          onClick={() => setCurrentLocation("America/Los_Angeles")}
+        >
+          Los Angeles
+        </button>
+        <button
+          className="btn"
+          onClick={() => setCurrentLocation("America/New_York")}
+        >
+          New York
+        </button>
+        <button
+          className="btn"
+          onClick={() => setCurrentLocation("Europe/Madrid")}
+        >
+          Spain
+        </button>
+        <button
+          className="btn"
+          onClick={() => setCurrentLocation("Asia/Kolkata")}
+        >
+          India
+        </button>
       </div>
-      <h2>New York, USA</h2>
-      <div className="clock-row">
-        {generateData({ offset: 3 }).map((employee) => (
-          <ClockWithOffset
-            key={employee.name}
-            offset="America/New_York"
-            abbrev="ny"
-            name={employee.name}
-            startHours={employee.startHours}
-            endHours={employee.endHours}
-            hoursAvailable={employee.hoursAvailable}
-          />
-        ))}
-      </div>
-      <h2>Seville, Spain</h2>
-      <div className="clock-row">
-        {generateData({ offset: 9 }).map((employee) => (
-          <ClockWithOffset
-            key={employee.name}
-            offset="Europe/Madrid"
-            abbrev="eu"
-            name={employee.name}
-            startHours={employee.startHours}
-            endHours={employee.endHours}
-            hoursAvailable={employee.hoursAvailable}
-          />
-        ))}
-      </div>
-      <h2>Bengaluru, India</h2>
-      <div className="clock-row">
-        {generateData({ offset: 13 }).map((employee) => (
-          <ClockWithOffset
-            key={employee.name}
-            offset="Asia/Kolkata"
-            abbrev="as"
-            name={employee.name}
-            startHours={employee.startHours}
-            endHours={employee.endHours}
-            minutesOffset={30}
-            hoursAvailable={employee.hoursAvailable}
-          />
-        ))}
-      </div>
+      {renderRow()}
     </div>
   );
 }
